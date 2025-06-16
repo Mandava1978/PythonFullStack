91aec9869e2edb8eac9ec8d4c7119ffa5224bbaf
MANDAVA SRINIVASARAO <150275277+Mandava1978@users.noreply.github.com>
2025-06-16 12:58:38 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,8 @@
+firstnumber=int(input("Enter the first number: "))
+secondnumber=int(input("Enter the second number: "))
+
+
+print("\nAddition:" ,firstnumber+secondnumber)
+print("Subtraction:" ,firstnumber-secondnumber)
+print("Multiplication:" ,firstnumber*secondnumber)
+print("Division:" ,firstnumber/secondnumber)
